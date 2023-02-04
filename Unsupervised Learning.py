c59d30e9d4ea5572ef6310f0a1421b53241b449e
Patreeqiang <60885586+Alex-Duan-0001@users.noreply.github.com>
2023-02-04 12:18:23 +0100

    Update Unsupervised Learning.py

--- Unsupervised Learning.py
+++ Unsupervised Learning.py
@@ -5,14 +5,13 @@ import torchvision
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-import torch.nn.functional as F
 from tensorflow import keras
 from tqdm import tqdm   #the training progress show
 from torch.optim import Adam, Rprop #the learning rules for the weight optimzation
 from torch.nn.functional import normalize
 from torchvision import datasets
 from torchvision.datasets import MNIST
-
+import torch.nn.functional as F
 from torchvision.transforms import Compose, ToTensor, Normalize, Lambda # lambda self defined 
 
 
@@ -21,24 +20,24 @@ train_loader = torch.utils.data.DataLoader(
   torchvision.datasets.MNIST('./files/', train=True, download=True,
                              transform=torchvision.transforms.Compose([
                                torchvision.transforms.ToTensor(),
-                               torchvision.transforms.Normalize(  (0.1307,), (0.3081,)),
-                               
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
                              ])),
-  batch_size=128, shuffle=True)
+  batch_size=100, shuffle=True)
 
 test_loader = torch.utils.data.DataLoader(
   torchvision.datasets.MNIST('./files/', train=False, download=True,
                              transform=torchvision.transforms.Compose([
                                torchvision.transforms.ToTensor(),
-                               torchvision.transforms.Normalize((0.1307,), (0.3081,)),
-                               
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
                              ])),
-  batch_size=1000, shuffle=True)
+  batch_size=100, shuffle=True)
+
 
 # %%
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train.shape, x_test.shape
-
 
 # %%
 random_ex = np.random.rand(28,28)
@@ -188,12 +187,13 @@ x_train = (x_train-33.31002426147461 )/78.56748962402344
 x_train = x_train.reshape(x_train.shape[0], -1)
 y_train = y_train.reshape(y_train.shape[0])
 x = torch.tensor(x_train, dtype=torch.float)
-y = torch.tensor(y_train, dtype=torch.int)
+y = torch.tensor(y_train, dtype=torch.float)
 
 x_pos, x_neg = x_pos.cuda(device), x_neg.cuda(device)
 x_te, y_te = x_te.cuda(device), y_te.cuda(device)
 x, y = x.cuda(device), y.cuda(device)
 
+print(x.shape, y.shape, x.dtype)
 
 # %%
 class Layer(nn.Linear):
@@ -202,8 +202,8 @@ class Layer(nn.Linear):
         super().__init__(in_features, out_features, bias, device, dtype)
         self.relu = torch.nn.ReLU()
         self.opt = Adam(self.parameters(), lr=0.06)
-        self.threshold = 2.0
-        self.num_epochs = 60
+        self.threshold = 2
+        self.num_epochs = 100
 
     def forward(self, x):
         x_direction = x / (x.norm(2, 1, keepdim=True) + 0.02)
@@ -212,8 +212,11 @@ class Layer(nn.Linear):
 
     def train_layer(self, x_pos, x_neg):
         for i in tqdm(range(self.num_epochs)):
-            g_pos = self.forward(x_pos).pow(2).mean(1)
-            g_neg = self.forward(x_neg).pow(2).mean(1)
+          for b in range (60):
+            g_pos = self.forward(x_pos[b*1000: (b+1)*1000]).pow(2).mean(1)
+            g_neg = self.forward(x_neg[b*1000: (b+1)*1000]).pow(2).mean(1)
+            #g_pos = self.forward(x_pos).pow(2).mean(1)
+            #g_neg = self.forward(x_neg).pow(2).mean(1)
             # The following loss pushes pos (neg) samples to
             # values larger (smaller) than the self.threshold.
             loss = torch.log(1 + torch.exp(torch.cat([
@@ -226,6 +229,7 @@ class Layer(nn.Linear):
             self.opt.step()
         return self.forward(x_pos).detach(), self.forward(x_neg).detach()
 
+# %%
 
 class FFNet(torch.nn.Module):
 
@@ -246,67 +250,138 @@ class FFNet(torch.nn.Module):
 
 
 # %%
-net = FFNet()
-net.train_ffnet(x_pos, x_neg)
+(x_train2, y_train2), (x_test2, y_test2) = tf.keras.datasets.mnist.load_data()
 
+def edit_data(x, y, method="edit"):
+    is_batch = x.ndim == 3
+    if method == "edit":
+        if is_batch:
+            x[:, 0, :10] = 0.0
+            for i in range(x.shape[0]):
+                x[i, 0, y[i]] = 255
+        else:
+            x[0, :10] = 0.0
+            x[0, y] = 255
+
+def random_label(y):
+    if type(y) != np.ndarray:
+        label = list(range(10))
+        del label[y]
+        return np.random.choice(label)
+    else:
+        label = np.copy(y)
+        for i in range(y.shape[0]):
+            label[i] = random_label(y[i])
+        return label
+
+pos2 = np.copy(x_train2)
+neg2 = np.copy(x_train2)
+edit_data(pos2, y_train2)
+edit_data(neg2, random_label(y_train2))
+
+
+pos2 = (pos2-33.31002426147461 )/78.56748962402344
+neg2 = (neg2-33.31002426147461 )/78.56748962402344
+pos2 = pos2.reshape(pos2.shape[0], -1)
+neg2 = neg2.reshape(neg2.shape[0], -1)
+x_pos2 = torch.tensor(pos2, dtype=torch.float)
+x_neg2 = torch.tensor(neg2, dtype=torch.float)
+
+x_pos2, x_neg2 = x_pos.cuda(device), x_neg.cuda(device)
+
+
+# %%
+net = FFNet()
+net.train_ffnet(x_pos2, x_neg2)
 
 def freeze(model):
     for param in model.parameters():
         param.requires_grad = False
 
 # %%
+net.hlayer1.weight
+net.hlayer2.weight
+net.hlayer3.weight
+net.hlayer4.weight
+
+# %%
+
+h1 = net.hlayer1(x)
+h2 = net.hlayer2(h1)
+h3 = net.hlayer3(h2)
+h4 = net.hlayer4(h3)
+
+h1.shape, h2.shape, h3.shape, h4.shape
+
+
+# %%
+h5= torch.cat((h2,h3,h4), 1)
+print(h5.shape, h5,  h5.size(0))
+h6= h5.view(h5.size(0), -1)
+print(h6.shape, h6)
+
+# %%
 class Net(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(Net, self).__init__()
         
         self.hlayer1 = net.hlayer1
         self.hlayer2 = net.hlayer2
         self.hlayer3 = net.hlayer3
         self.hlayer4 = net.hlayer4
-      
-        freeze(self)
-             
+        freeze(self)     
         self.fc = nn.Linear(6000, 10).cuda(device)
-    
+        
     def forward(self, x):
+        x = torch.flatten(x, 1)
         n1= self.hlayer1(x)
         n2= self.hlayer2(n1)
         n3= self.hlayer3(n2)
         n4= self.hlayer4(n3)
-
+        n2 = n2 / (n2.norm(2, 1, keepdim=True) + 0.01)
+        n3 = n3 / (n2.norm(2, 1, keepdim=True) + 0.01)
+        n4 = n4 / (n2.norm(2, 1, keepdim=True) + 0.01)
 
         n5= torch.cat((n2,n3,n4), 1)
         n5 = n5.view(n5.size(0), -1)
         
         output = self.fc(n5)
-
+        
         return output 
         
 network = Net()
 print(network.fc)
 
+
 # %%
 criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(network.parameters(), lr = 0.01)
-#optimizer = torch.optim.Adam(filter(lambda p : p.requires_grad, net.parameters()), lr = 0.01)
+#optimizer = torch.optim.Adam(network.parameters(), lr = 0.01)
+optimizer = torch.optim.Adam(filter(lambda p : p.requires_grad, network.parameters()), lr = 0.01)
+
 # %%
-train_losses = []
-train_counter = []
+output = network(x)
+output.shape
+
+# %%
 n_epochs = 1
-batch_size_train = 60
+batch_size_train = 100
 batch_size_test = 1000
 
 # %%
-def train_bp(epoch):
+train_losses = []
+train_counter = []
+test_losses = []
+test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+
+# %%
+def train(epoch):
   network.train()
-  
   for batch_idx, (data, target) in enumerate(train_loader):
-    data = data.cuda(device)
-    target = data.cuda(device)
-    data = data.reshape(data.shape[0], -1)
+    data =data.cuda(device)
+    target = target.cuda(device)
     optimizer.zero_grad()
     output = network(data)
-  
     loss = criterion(output, target)
     loss.backward()
     if batch_idx % 10 == 0:
@@ -319,31 +394,40 @@ def train_bp(epoch):
       train_counter.append(
         (batch_idx*batch_size_train) + ((epoch-1)*len(train_loader.dataset)))
       torch.save(network.state_dict(), './results/model.pth')
-      torch.save(optimizer.state_dict(), './results/optimizer.pth') 
+      torch.save(optimizer.state_dict(), './results/optimizer.pth')
          
-# %%
-network.train()
-optimizer.zero_grad()
-output = network(x)
-print('output', output)
 
 # %%
-def train_examples(epoch):
-  
-  network.train()
-  optimizer.zero_grad()
-  output = network(x)
-  output = torch.tensor(output, dtype=int)
-  loss = criterion(output, y)
-  loss.backward()
-  
-  print('Train Epoch: {}  Loss: {:.6f}'.format(epoch, loss.item()))
-      
-  train_losses.append(loss.item())
-  torch.save(network.state_dict(), './results/model.pth')
-  torch.save(optimizer.state_dict(), './results/optimizer.pth') 
+for i in range (n_epochs):
+    train(i)
 
 # %%
-train_examples(0)
-train_bp(0)
+net.hlayer1.weight
+net.hlayer2.weight
+net.hlayer3.weight
+net.hlayer4.weight
+
+# %%
+def test():
+  network.eval()
+  test_loss = 0
+  correct = 0
+  with torch.no_grad():
+    for data, target in test_loader:
+      data =data.cuda(device)
+      target = target.cuda(device)
+      #print(data.dtype, target.shape)
+      output = network(data)
+      test_loss += criterion(output, target).item()
+      pred = output.data.max(1, keepdim=True)[1]
+      correct += pred.eq(target.data.view_as(pred)).sum()
+  test_loss /= len(test_loader.dataset)
+  test_losses.append(test_loss)
+  print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    test_loss, correct, len(test_loader.dataset),
+    100. * correct / len(test_loader.dataset)))
+
+
+# %%
+test()
 
